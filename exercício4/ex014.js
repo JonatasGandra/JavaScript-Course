a036af3cce9c55a5702d39a88ce767e459f60cea
Jonatas Santos Gandra <jonatassgandra@gmail.com>
2024-12-08 00:34:35 -0300

    add value. select -> option

--- exercício4/ex014.js
+++ exercício4/ex014.js
@@ -7,12 +7,14 @@ function gerar(){
     let n = Number(ntxt.value)
     let i = 0
     let tab = document.querySelector('#tabuada')
-    if(n.length == 0){
+    if(ntxt.value.length == 0){
         window.alert('por favor, digite um número')
     }else{
        for(i; i<=10; i++){
         let item = document.createElement('option')
         item.text = `${i} x ${n} = ${i*n}`
+        
+        item.value = `tab${i}`// ou item.setAttribute('value', `tab${i}`)
         tab.appendChild(item)
     } 
     }
