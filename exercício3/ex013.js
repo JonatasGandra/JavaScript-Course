ad05fe39e2b01a51dee75e0e9a125840294f1275
Jonatas Santos Gandra <jonatassgandra@gmail.com>
2024-12-07 23:25:40 -0300

    loop JS

---
+++ exercício3/ex013.js
@@ -0,0 +1,25 @@
+var c = window.document.querySelector('#contar')
+c.addEventListener('click', contar)
+
+function contar(){
+
+    var is = window.document.querySelector( '#inicio')
+    var fs = window.document.querySelector( '#fim')
+    var ps = window.document.querySelector( '#passos')
+    var i = Number(is.value)
+    var f = Number(fs.value)
+    var p = Number(ps.value)
+    var res = window.document.querySelector('#res')
+    res.innerHTML = ''
+    
+    if(i<f){
+        for(i; i<=f; i+=p){  
+            res.innerHTML += (`${i} -> `)
+    }
+    }else{
+    for(i; i>=f; i-=p){
+            res.innerHTML += (`${i} -> `)
+    }
+}
+res.innerHTML += 'fim!'
+}
